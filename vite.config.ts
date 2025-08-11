4efb1122d4e74926ffc95fdae8233a90acedef71
Colectivo Tombola <colectivotombola@gmail.com>
2025-08-10 21:40:33 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/FAGESAS/",   // 👈 clave para GitHub Pages
+  base: "/FAGESAS/",   // clave para GitHub Pages
   server: {
     host: "::",
     port: 8080,
